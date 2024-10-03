ad30aff3faaf6218ed7465b0a9b23848bf749fe7
mmahdin <mahdi9roozi@gmail.com>
2024-10-04 03:09:15 +0330

    lab3 -> write fully connected network with variable layer

---
+++ lab/3/main.py
@@ -0,0 +1,124 @@
+class Linear(object):
+
+    @staticmethod
+    def forward(x, w, b):
+        """
+        Computes the forward pass for an linear (fully-connected) layer.
+        The input x has shape (N, d_1, ..., d_k) and contains a minibatch of N
+        examples, where each example x[i] has shape (d_1, ..., d_k). We will
+        reshape each input into a vector of dimension D = d_1 * ... * d_k, and
+        then transform it to an output vector of dimension M.
+        Inputs:
+        - x: A tensor containing input data, of shape (N, d_1, ..., d_k)
+        - w: A tensor of weights, of shape (D, M)
+        - b: A tensor of biases, of shape (M,)
+        Returns a tuple of:
+        - out: output, of shape (N, M)
+        - cache: (x, w, b)
+        """
+        N = x.shape[0]
+        transform = x.reshape((N, -1))
+        out = torch.mm(transform, w) + b
+        cache = (x, w, b)
+        return out, cache
+
+    @staticmethod
+    def backward(dout, cache):
+        """
+        Computes the backward pass for an linear layer.
+        Inputs:
+        - dout: Upstream derivative, of shape (N, M)
+        - cache: Tuple of:
+          - x: Input data, of shape (N, d_1, ... d_k)
+          - w: Weights, of shape (D, M)
+          - b: Biases, of shape (M,)
+        Returns a tuple of:
+        - dx: Gradient with respect to x, of shape
+          (N, d1, ..., d_k)
+        - dw: Gradient with respect to w, of shape (D, M)
+        - db: Gradient with respect to b, of shape (M,)
+        """
+        x, w, b = cache
+        dx, dw, db = None, None, None
+
+        N = x.shape[0]
+
+        # Reshape x to (N, D)
+        input_vect = x.reshape((N, -1))
+
+        # Gradient with respect to input x
+        dx = dout @ w.T
+        dx = dx.view(*x.shape)
+
+        # Gradient with respect to weights w
+        dw = input_vect.T @ dout
+
+        # Gradient with respect to biases b
+        db = dout.sum(dim=0)
+
+        return dx, dw, db
+
+
+class ReLU(object):
+
+    @staticmethod
+    def forward(x):
+        """
+        Computes the forward pass for a layer of rectified
+        linear units (ReLUs).
+        Input:
+        - x: Input; a tensor of any shape
+        Returns a tuple of:
+        - out: Output, a tensor of the same shape as x
+        - cache: x
+        """
+        out = x.clamp(min=0)
+        cache = x
+        return out, cache
+
+    @staticmethod
+    def backward(dout, cache):
+        """
+        Computes the backward pass for a layer of rectified
+        linear units (ReLUs).
+        Input:
+        - dout: Upstream derivatives, of any shape
+        - cache: Input x, of same shape as dout
+        Returns:
+        - dx: Gradient with respect to x
+        """
+        dx, x = None, cache
+        dx = dout.clone()
+        dx[x <= 0] = 0
+        return dx
+
+
+class Linear_ReLU(object):
+
+    @staticmethod
+    def forward(x, w, b):
+        """
+        Convenience layer that performs an linear transform
+        followed by a ReLU.
+
+        Inputs:
+        - x: Input to the linear layer
+        - w, b: Weights for the linear layer
+        Returns a tuple of:
+        - out: Output from the ReLU
+        - cache: Object to give to the backward pass
+        """
+        a, fc_cache = Linear.forward(x, w, b)
+        out, relu_cache = ReLU.forward(a)
+        cache = (fc_cache, relu_cache)
+        return out, cache
+
+    @staticmethod
+    def backward(dout, cache):
+        """
+        Backward pass for the linear-relu convenience layer
+        """
+        fc_cache, relu_cache = cache
+        da = ReLU.backward(dout, relu_cache)
+        dx, dw, db = Linear.backward(da, fc_cache)
+        return dx, dw, db
